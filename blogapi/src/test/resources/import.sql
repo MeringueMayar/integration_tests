445d1c2bd3c191b08719a5da6518c001132cdc54
Sebastian Piecowski <202268@edu.p.lodz.pl>
2018-05-28 17:53:41 +0200

    Dodano userow i posty do import.sql

--- blogapi/src/test/resources/import.sql
+++ blogapi/src/test/resources/import.sql
@@ -1,5 +1,1 @@
 --this script initiates db for integration tests 
-insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'john@domain.com', 'John', 'Steward')
-insert into user (id, account_status, email, first_name) values (null, 'NEW', 'brian@domain.com', 'Brian')
-insert into blog_post values (null, 'blog-test', 1)
-insert into blog_post values (null, 'blog-test', 2)
