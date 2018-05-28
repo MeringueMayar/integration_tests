d41601fc6439fd26acd963568e86751e10147464
dawid.liwocha <202214@edu.p.lodz.pl>
2018-05-28 12:06:07 +0200

    added test for likingPost by not CONFIRMED user

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -12,8 +12,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import edu.iis.mto.blog.api.request.UserRequest;
+import edu.iis.mto.blog.domain.errors.DomainError;
 import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
 import edu.iis.mto.blog.domain.model.User;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
 import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.DataMapper;
 import edu.iis.mto.blog.services.BlogService;
@@ -25,6 +28,9 @@ public class BlogManagerTest {
     @MockBean
     UserRepository userRepository;
 
+    @MockBean
+    BlogPostRepository postRepository;
+    
     @Autowired
     DataMapper dataMapper;
 
@@ -38,6 +44,23 @@ public class BlogManagerTest {
         Mockito.verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+    
+    @Test(expected = DomainError.class)
+    public void addingLikeToPostByNotConfirmedUserShouldThrowDomainError() {
+        Long postAndOwnerId = new Long(1);
+        Long likingUserId = new Long(2);
+        User likingUser = new User();
+        likingUser.setId(likingUserId);
+        likingUser.setAccountStatus(AccountStatus.NEW);
+        User postOwner = new User();
+        postOwner.setId(postAndOwnerId);
+        BlogPost post = new BlogPost();
+        post.setId(postAndOwnerId);
+        post.setUser(postOwner);
+        Mockito.when(userRepository.findOne(likingUserId)).thenReturn(likingUser);      
+        Mockito.when(postRepository.findOne(postAndOwnerId)).thenReturn(post);
+        blogService.addLikeToPost(likingUserId, postAndOwnerId);
     }
 
 }
