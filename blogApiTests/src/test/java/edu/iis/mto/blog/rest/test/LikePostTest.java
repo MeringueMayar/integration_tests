ece7de7f38162d35fcd04f6005ab4755132a8874
Sebastian Piecowski <202268@edu.p.lodz.pl>
2018-05-28 17:47:40 +0200

    Dodano test: polubienie wlasnego postu, wielokrotne polubienie postu

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -1,5 +1,7 @@
 package edu.iis.mto.blog.rest.test;
 
+import static org.hamcrest.CoreMatchers.hasItem;
+
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
 import org.junit.Test;
@@ -8,8 +10,11 @@ import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 
 public class LikePostTest {
-	private final String confirmedUserLikePost = "blog/user/1/like/2";
-	private final String unconfirmedUserLikePost = "blog/user/3/like/2";
+	private final String confirmedUserLikePost = "blog/user/1/like/3";
+	private final String unconfirmedUserLikePost = "blog/user/2/like/1";
+	private final String likeOwnPost = "blog/user/1/like/1";
+	private final String multipleLikedPost = "blog/user/3/post";
+
 	@Test
 	public void whenConfirmedUserAddLikeToPostShouldReturnStatusOK() {
 		JSONObject jsonObj = new JSONObject();
@@ -17,11 +22,32 @@ public class LikePostTest {
 				.body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
 				.post(confirmedUserLikePost);
 	}
+
 	@Test
-    public void shouldReturnHttpStatusConflictIfUnonfirmedUserIsLikingOtherUserPost() {
-        JSONObject jsonObj = new JSONObject();
-        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST)
-                .when().post(unconfirmedUserLikePost);
-    }
+	public void whenUnconfirmedUserAddLikeToPostShouldReturnStatusBadRequest() {
+		JSONObject jsonObj = new JSONObject();
+		RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+				.body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+				.post(unconfirmedUserLikePost);
+	}
+
+	@Test
+	public void whenUserTryToLikeOwnPostShouldReturnStatusConflict() {
+		JSONObject jsonObj = new JSONObject();
+		RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+				.body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+				.post(likeOwnPost);
+	}
+
+	@Test
+	public void whenUserMultipleLikePostStatusShouldStayTheSame() {
+		JSONObject jsonObj = new JSONObject();
+		RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+				.body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+				.post(confirmedUserLikePost);
+		RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+				.body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+				.post(confirmedUserLikePost);
+		RestAssured.given().when().get(multipleLikedPost).then().assertThat().body("likesCount", hasItem(1));
+	}
 }
---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchingPostTest.java
@@ -0,0 +1,17 @@
+package edu.iis.mto.blog.rest.test;
+
+import org.apache.http.HttpStatus;
+
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+
+
+public class SearchingPostTest {
+	private final String removedUser="/blog/user/4/post";
+	@Test
+	public void whenUserStatusRemovedSearchingShouldReturnBadRequest() {
+		RestAssured.when().get(removedUser).then().contentType(ContentType.JSON).statusCode(HttpStatus.SC_BAD_REQUEST);
+	}
+}
