98df0b41c720fbee3c6555928f67f2a2b888d215
dawid.liwocha <202214@edu.p.lodz.pl>
2018-05-28 13:29:58 +0200

    added 2 blogposts to import.sql and added functional test for liking post by confirmed and new user

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -0,0 +1,32 @@
+package edu.iis.mto.blog.rest.test;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+
+public class LikePostTest extends FunctionalTests {
+
+    private static final String CONFIRMED_USER_LIKE_API = "/blog/user/1/like/2";
+    
+    private static final String NEW_USER_LIKE_API = "/blog/user/2/like/1";
+    
+    @Test
+    public void shouldReturnHttpStatusOkIfConfirmedUserIsLikingOtherUserPost() {
+        JSONObject jsonObj = new JSONObject();
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK)
+                .when().post(CONFIRMED_USER_LIKE_API);
+    }
+    
+    @Test
+    public void shouldReturnHttpStatusBadRequestIfNewUserIsLikingPost() {
+        JSONObject jsonObj = new JSONObject();
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when().post(NEW_USER_LIKE_API);
+    }
+    
+}
--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -1,3 +1,5 @@
 --this script initiates db for h2 db (used in test profile)
 insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'john@domain.com', 'John', 'Steward')
 insert into user (id, account_status, email, first_name) values (null, 'NEW', 'brian@domain.com', 'Brian')
+insert into blog_post values (null, 'test', 1)
+insert into blog_post values (null, 'test', 2)
