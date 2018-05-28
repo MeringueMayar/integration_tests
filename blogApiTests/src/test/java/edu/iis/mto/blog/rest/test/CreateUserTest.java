1cb4cf7909e29f9328f55341f6f794c4c3fa35d4
Sebastian Piecowski <202268@edu.p.lodz.pl>
2018-05-28 18:29:41 +0200

    Czyszczenie kodu

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -9,12 +9,12 @@ import com.jayway.restassured.http.ContentType;
 
 public class CreateUserTest extends FunctionalTests {
 
-    private static final String USER_API = "/blog/user";
+	private static final String USER_API = "/blog/user";
 
-    @Test
-    public void postFormWithNotUniqueDataShouldReturnStatusConflict() {
-        JSONObject jsonObj = new JSONObject().put("email", "brian@domain.com");
-        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CONFLICT).when().post(USER_API);
-    }
+	@Test
+	public void postFormWithNotUniqueDataShouldReturnStatusConflict() {
+		JSONObject jsonObj = new JSONObject().put("email", "brian@domain.com");
+		RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+				.body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CONFLICT).when().post(USER_API);
+	}
 }
