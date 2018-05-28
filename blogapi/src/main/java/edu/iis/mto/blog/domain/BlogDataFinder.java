57c351af0679ec575bc086bb7ff1de6f5c98557a
Sebastian Piecowski <202268@edu.p.lodz.pl>
2018-05-28 18:27:37 +0200

    Dodano brakujace zabezpieczenia

--- blogapi/src/main/java/edu/iis/mto/blog/domain/BlogDataFinder.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/BlogDataFinder.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.blog.domain;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.AccountStatus;
 import edu.iis.mto.blog.domain.model.BlogPost;
 import edu.iis.mto.blog.domain.model.User;
 import edu.iis.mto.blog.dto.PostData;
@@ -19,34 +22,42 @@ import edu.iis.mto.blog.services.DataFinder;
 @Service
 public class BlogDataFinder extends DomainService implements DataFinder {
 
-    @Override
-    public UserData getUserData(Long userId) {
-        User user = userRepository.findOne(userId);
-        if (user == null) {
-            throw new EntityNotFoundException(String.format("user with id %1 does not exists", userId));
-        }
-        return mapper.mapToDto(user);
-    }
+	@Override
+	public UserData getUserData(Long userId) {
+		User user = userRepository.findOne(userId);
+		if (user == null) {
+			throw new EntityNotFoundException(String.format("user with id %1 does not exists", userId));
+		}
+		return mapper.mapToDto(user);
+	}
 
-    @Override
-    public List<UserData> findUsers(String searchString) {
-        List<User> users = userRepository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
-                searchString, searchString, searchString);
+	@Override
+	public List<UserData> findUsers(String searchString) {
+		List<User> users = userRepository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+				searchString, searchString, searchString);
+		Iterator itr=users.iterator();
+		while(itr.hasNext()) {
+			User user=(User) itr.next();
+			if(user.getAccountStatus()==AccountStatus.REMOVED)
+				itr.remove();
+		}
+		return users.stream().map(user -> mapper.mapToDto(user)).collect(Collectors.toList());
+	}
 
-        return users.stream().map(user -> mapper.mapToDto(user)).collect(Collectors.toList());
-    }
+	@Override
+	public PostData getPost(Long userId) {
+		BlogPost blogPost = blogPostRepository.findOne(userId);
+		return mapper.mapToDto(blogPost);
+	}
 
-    @Override
-    public PostData getPost(Long userId) {
-        BlogPost blogPost = blogPostRepository.findOne(userId);
-        return mapper.mapToDto(blogPost);
-    }
-
-    @Override
-    public List<PostData> getUserPosts(Long userId) {
-        User user = userRepository.findOne(userId);
-        List<BlogPost> posts = blogPostRepository.findByUser(user);
-        return posts.stream().map(post -> mapper.mapToDto(post)).collect(Collectors.toList());
-    }
+	@Override
+	public List<PostData> getUserPosts(Long userId) {
+		User user = userRepository.findOne(userId);
+		if (user.getAccountStatus() == AccountStatus.REMOVED) {
+			throw new DomainError("account removed");
+		}
+		List<BlogPost> posts = blogPostRepository.findByUser(user);
+		return posts.stream().map(post -> mapper.mapToDto(post)).collect(Collectors.toList());
+	}
 
 }
