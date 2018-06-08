207a6f483708ee131458003e6d2461514749171f
Famulski.Michal <187684@edu.p.lodz.pl>
2018-06-08 14:21:34 +0200

    3.2 Added test case for adding like by confirmed user

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,5 +1,8 @@
 package edu.iis.mto.blog.domain;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,7 +16,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import edu.iis.mto.blog.api.request.UserRequest;
 import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.model.User;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.DataMapper;
 import edu.iis.mto.blog.services.BlogService;
@@ -24,6 +31,12 @@ public class BlogManagerTest {
 
     @MockBean
     UserRepository userRepository;
+
+    @MockBean
+    BlogPostRepository blogPostRepository;
+
+    @MockBean
+    LikePostRepository likePostRepository;
 
     @Autowired
     DataMapper dataMapper;
@@ -38,6 +51,51 @@ public class BlogManagerTest {
         Mockito.verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+
+    @Test
+    public void confirmedUserShouldBeAbleToLikePost() {
+        User blogPostAuthor = setUpBlogPostAuthor();
+        User blogUser = setUpBlogUser();
+        BlogPost post = setUpBlogPost(blogPostAuthor);
+        Optional<LikePost> likes = Optional.empty();
+
+        Mockito.when(userRepository.findOne(blogPostAuthor.getId())).thenReturn(blogPostAuthor);
+        Mockito.when(userRepository.findOne(blogUser.getId())).thenReturn(blogUser);
+        Mockito.when(blogPostRepository.findOne(post.getId())).thenReturn(post);
+        Mockito.when(likePostRepository.findByUserAndPost(blogUser, post)).thenReturn(likes);
+
+        boolean success = blogService.addLikeToPost(blogUser.getId(), post.getId());
+        Assert.assertThat(success, Matchers.equalTo(true));
+    }
+
+    private User setUpBlogPostAuthor() {
+        User blogPostAuthor = new User();
+        blogPostAuthor.setAccountStatus(AccountStatus.CONFIRMED);
+        blogPostAuthor.setFirstName("Jan");
+        blogPostAuthor.setLastName("Nowak");
+        blogPostAuthor.setEmail("john@domain.com");
+        blogPostAuthor.setId(1L);
+        return blogPostAuthor;
+    }
+
+    private User setUpBlogUser() {
+        User blogUser = new User();
+        blogUser.setAccountStatus(AccountStatus.CONFIRMED);
+        blogUser.setFirstName("Tomasz");
+        blogUser.setLastName("Kowalski");
+        blogUser.setEmail("tom@domain.com");
+        blogUser.setId(2L);
+        return blogUser;
+    }
+
+    private BlogPost setUpBlogPost(User blogPostAuthor) {
+        BlogPost blogPost = new BlogPost();
+        blogPost.setEntry("Nowy wpis");
+        blogPost.setUser(blogPostAuthor);
+        blogPost.setLikes(new ArrayList<LikePost>());
+        blogPost.setId(3L);
+        return blogPost;
     }
 
 }
