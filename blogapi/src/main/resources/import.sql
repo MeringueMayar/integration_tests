fc9bae0257fdfe1bba4b121c92d51b8a0603f6c0
Famulski.Michal <187684@edu.p.lodz.pl>
2018-06-14 21:34:33 +0200

    3.4.3 Add like to post test

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -1,3 +1,6 @@
 --this script initiates db for h2 db (used in test profile)
 insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'john@domain.com', 'John', 'Steward')
 insert into user (id, account_status, email, first_name) values (null, 'NEW', 'brian@domain.com', 'Brian')
+insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'tom@domain.com', 'Tom', 'Hopkins')
+insert into blog_post (user_id, entry, id) values (1, 'nowy post11', null)
+insert into blog_post (user_id, entry, id) values (1, 'nowy post22', null)
