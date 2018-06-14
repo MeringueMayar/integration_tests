148f8e61362a63e066a6ada8973a0475048b7e48
Famulski.Michal <187684@edu.p.lodz.pl>
2018-06-14 22:18:32 +0200

    3.4.5 Get users data tests

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -3,6 +3,7 @@ insert into user (id, account_status, email, first_name, last_name) values (null
 insert into user (id, account_status, email, first_name) values (null, 'NEW', 'brian@domain.com', 'Brian')
 insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'tom@domain.com', 'Tom', 'Hopkins')
 insert into user (id, account_status, email, first_name, last_name) values (null, 'REMOVED', 'dean@domain.com', 'Dean', 'Winchester')
+insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'Sam@domain.com', 'Sam', 'Winchester')
 
 insert into blog_post (user_id, entry, id) values (1, 'nowy post11', null)
 insert into blog_post (user_id, entry, id) values (1, 'nowy post22', null)
