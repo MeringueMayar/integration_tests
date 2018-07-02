1406757f07f0ebb8017a8e1cec68a1897b31b110
Artur Jankowski <artij1@o2.pl>
2018-07-02 23:56:38 +0200

    No post user search returns no post test added

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchPostsTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchPostsTest.java
@@ -10,10 +10,17 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class SearchPostsTest {
     private static final String POST_BY_USER_URL = "/blog/user/4/post";
+    private static final String URL_OF_USER_WITHOT_POSTS = "/blog/user/2/post";
 
     @Test
     public void shouldReturnCorrectNumberOfPostsWhenValidUserIsProvided() {
         JSONObject jsonObj = new JSONObject();
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8").body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).and().body("size()", is(1)).when().get(POST_BY_USER_URL);
+    }
+
+    @Test
+    public void shouldReturnNothingWhenUserWithNoPostsIsProvided() {
+        JSONObject jsonObj = new JSONObject();
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8").body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).and().body("size()", is(0)).when().get(URL_OF_USER_WITHOT_POSTS);
     }
 }
