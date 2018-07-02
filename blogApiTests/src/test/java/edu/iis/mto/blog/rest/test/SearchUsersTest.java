36ceabba9a7f70ce1f82690ddd143b0482381521
Artur Jankowski <artij1@o2.pl>
2018-07-02 23:36:14 +0200

    Added test if searching user by non-existent id not returns user

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchUsersTest.java
@@ -0,0 +1,18 @@
+package edu.iis.mto.blog.rest.test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class SearchUsersTest {
+    private static final String BASE_URL = "/blog/user/";
+
+    @Test
+    public void shouldntReturnNonExistedUser() {
+        JSONObject jsonObj = new JSONObject();
+        String userId = "0";
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8").body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_NOT_FOUND).when().get(BASE_URL + userId);
+    }
+}
