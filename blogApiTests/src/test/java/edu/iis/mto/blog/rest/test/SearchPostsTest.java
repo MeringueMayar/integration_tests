343afba283da0f951ecf73b2fc8a0749900ccbc6
Artur Jankowski <artij1@o2.pl>
2018-07-02 23:50:43 +0200

    Added searching test to find post by user

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchPostsTest.java
@@ -0,0 +1,19 @@
+package edu.iis.mto.blog.rest.test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class SearchPostsTest {
+    private static final String POST_BY_USER_URL = "/blog/user/4/post";
+
+    @Test
+    public void shouldReturnCorrectNumberOfPostsWhenValidUserIsProvided() {
+        JSONObject jsonObj = new JSONObject();
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8").body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).and().body("size()", is(1)).when().get(POST_BY_USER_URL);
+    }
+}
