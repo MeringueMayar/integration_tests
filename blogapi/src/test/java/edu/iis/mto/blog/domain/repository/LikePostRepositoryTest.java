3986a588586ed34c6e1d82372ff6d305becdaa03
Wiktor Pabich <202258@edu.p.lodz.pl>
2018-08-01 20:58:31 +0200

    likeRepositoryTest - tests completed

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -4,7 +4,9 @@ import edu.iis.mto.blog.domain.model.AccountStatus;
 import edu.iis.mto.blog.domain.model.BlogPost;
 import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.model.User;
+import org.hamcrest.Matchers;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,6 +15,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -53,5 +58,88 @@ public class LikePostRepositoryTest {
         singleLike.setUser(persistedUser);
     }
 
+    @Test
+    public void shouldNotFindLikes_givernLikePostRepository_isEmpty(){
+        List<LikePost> foundLikes = likePostRepository.findAll();
+        assertThat(foundLikes.size(), Matchers.equalTo(0));
+    }
 
+    @Test
+    public void shouldFindLike_givenLikePostRepository_withThisLike(){
+        LikePost persistedLike = entityManager.persist(singleLike);
+        List<LikePost> foundLikes = likePostRepository.findAll();
+
+        assertThat(foundLikes.get(0).getId(), Matchers.equalTo(persistedLike.getId()));
+    }
+
+    @Test
+    public void shouldAddLike_toPost(){
+        LikePost persistedLike = likePostRepository.save(singleLike);
+
+        Optional<LikePost> foundLikes = likePostRepository.findByUserAndPost(persistedUser, persistedPost);
+        assertThat(foundLikes.get(), Matchers.equalTo(persistedLike));
+    }
+
+    @Test
+    public void shouldNotFind_notAddedLike(){
+        LikePost persistedLike = likePostRepository.save(singleLike);
+
+        LikePost someOtherLike = new LikePost();
+        Optional<LikePost> foundLikes = likePostRepository.findByUserAndPost(persistedUser, persistedPost);
+        assertThat(foundLikes.get(), Matchers.not(Matchers.equalTo(someOtherLike)));
+    }
+
+    @Test
+    public void searchingWithIncorrectUserData_shouldNotFindLike(){
+        LikePost persistedLike = likePostRepository.save(singleLike);
+
+        User falseUser = new User();
+        falseUser.setFirstName("James");
+        falseUser.setLastName("Malinovsky");
+        falseUser.setEmail("123@interia.pl");
+        falseUser.setAccountStatus(AccountStatus.NEW);
+
+        entityManager.persist(falseUser);
+
+        Optional<LikePost> foundLikes = likePostRepository.findByUserAndPost(falseUser, persistedPost);
+        assertThat(foundLikes.isPresent(),Matchers.equalTo(false));
+    }
+
+    @Test
+    public void searchingWithIncorrectPostData_shouldNotFindLike(){
+        LikePost persistedLike = likePostRepository.save(singleLike);
+
+        BlogPost falseBlogPost = new BlogPost();
+        falseBlogPost.setUser(persistedUser);
+        falseBlogPost.setEntry("SOME_CONTENT_123_!@#");
+        falseBlogPost.setLikes(likesPost);
+
+        entityManager.persist(falseBlogPost);
+
+        Optional<LikePost> foundLikes = likePostRepository.findByUserAndPost(persistedUser, falseBlogPost);
+        assertThat(foundLikes.isPresent(),Matchers.equalTo(false));
+    }
+
+    @Test
+    public void searchingWithIncorrectUserAndPostData_shouldNotFindLike(){
+        LikePost persistedLike = likePostRepository.save(singleLike);
+
+        BlogPost falseBlogPost = new BlogPost();
+        falseBlogPost.setUser(persistedUser);
+        falseBlogPost.setEntry("SOME_CONTENT_123_!@#");
+        falseBlogPost.setLikes(likesPost);
+
+        entityManager.persist(falseBlogPost);
+
+        User falseUser = new User();
+        falseUser.setFirstName("James");
+        falseUser.setLastName("Malinovsky");
+        falseUser.setEmail("123@interia.pl");
+        falseUser.setAccountStatus(AccountStatus.NEW);
+
+        entityManager.persist(falseUser);
+
+        Optional<LikePost> foundLikes = likePostRepository.findByUserAndPost(persistedUser, falseBlogPost);
+        assertThat(foundLikes.isPresent(),Matchers.equalTo(false));
+    }
 }
