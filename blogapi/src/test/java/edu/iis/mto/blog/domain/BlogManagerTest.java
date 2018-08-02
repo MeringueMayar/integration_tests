f1a7f76675dc710f7d0397c6a79a4d7f151bf822
Wiktor Pabich <202258@edu.p.lodz.pl>
2018-08-02 20:33:30 +0200

    added BlogManagerTests and mechanism for letting to create and like posts only by confirmed accounts

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,7 +1,13 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -18,18 +24,51 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.DataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
 
     @MockBean
     UserRepository userRepository;
+    @MockBean
+    BlogPostRepository blogPostRepository;
+    @MockBean
+    LikePostRepository likePostRepository;
 
     @Autowired
     DataMapper dataMapper;
-
     @Autowired
     BlogService blogService;
+
+    private User blogPostUser;
+    private BlogPost blogPost;
+    private User blogPostAuthor;
+
+    @Before
+    public void setUp(){
+        blogPostAuthor = new User();
+        blogPostAuthor.setFirstName("John");
+        blogPostAuthor.setLastName("Lemon");
+        blogPostAuthor.setEmail("123@onet.pl");
+        blogPostAuthor.setAccountStatus(AccountStatus.CONFIRMED);
+        blogPostAuthor.setId(1L);
+
+        blogPostUser = new User();
+        blogPostUser.setFirstName("Mike");
+        blogPostUser.setLastName("Placek");
+        blogPostUser.setEmail("321333@onet.pl");
+        blogPostUser.setId(2L);
+
+        blogPost = new BlogPost();
+        blogPost.setEntry("CCONTENT_CONTENT");
+        blogPost.setUser(blogPostAuthor);
+        blogPost.setId(3L);
+        blogPost.setLikes(new ArrayList<LikePost>());
+    }
 
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
@@ -40,4 +79,31 @@ public class BlogManagerTest {
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
 
+    @Test
+    public void userWithConfirmedStatus_shouldAddLikePost(){
+        blogPostUser.setAccountStatus(AccountStatus.CONFIRMED);
+
+        Optional<LikePost> likes = Optional.empty();
+
+        Mockito.when(userRepository.findOne(blogPostAuthor.getId())).thenReturn(blogPostAuthor);
+        Mockito.when(userRepository.findOne(blogPostUser.getId())).thenReturn(blogPostUser);
+        Mockito.when(blogPostRepository.findOne(blogPost.getId())).thenReturn(blogPost);
+        Mockito.when(likePostRepository.findByUserAndPost(blogPostUser, blogPost)).thenReturn(likes);
+
+        blogService.addLikeToPost(blogPostUser.getId(), blogPost.getId());
+    }
+
+    @Test(expected = DomainError.class)
+    public void userWithNewStatus_shouldNotAddLikePost(){
+        blogPostUser.setAccountStatus(AccountStatus.NEW);
+
+        Optional<LikePost> likes = Optional.empty();
+
+        Mockito.when(userRepository.findOne(blogPostAuthor.getId())).thenReturn(blogPostAuthor);
+        Mockito.when(userRepository.findOne(blogPostUser.getId())).thenReturn(blogPostUser);
+        Mockito.when(blogPostRepository.findOne(blogPost.getId())).thenReturn(blogPost);
+        Mockito.when(likePostRepository.findByUserAndPost(blogPostUser, blogPost)).thenReturn(likes);
+
+        blogService.addLikeToPost(blogPostUser.getId(), blogPost.getId());
+    }
 }
