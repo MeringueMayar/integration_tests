5c8ea67a7af181e01dcce0a69bb5a30d1951e6f1
Wiktor Pabich <202258@edu.p.lodz.pl>
2018-08-06 22:58:30 +0200

    moved import records to correct import file

--- blogapi/src/test/resources/import.sql
+++ blogapi/src/test/resources/import.sql
@@ -1,7 +1,2 @@
-insert into blog_post (user_id, entry, id) values (1, 'CONTENT_CONTENT_CONTENT111', null)
-insert into blog_post (user_id, entry, id) values (1, 'CONTENT_CONTENT_CONTENT222', null)
-
-insert into user (id, account_status, email, first_name) values (null, 'NEW', 'itsmichel69@gmail.com', 'Brian')
-insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'jan@yahoo.com', 'Jan', 'Kowalski')
-insert into user (id, account_status, email, first_name, last_name) values (null, 'REMOVED', 'dean@domain.com', 'Maire', 'Yorke')
-insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'Sam@domain.com', 'Colin', 'Greenwood')
+--this script initiates db for integration tests
+--insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'john@domain.com', 'John', 'Steward')
