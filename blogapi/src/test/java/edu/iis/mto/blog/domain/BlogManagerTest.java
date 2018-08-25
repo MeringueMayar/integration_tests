431fdc3c44f97cc12ebd14eccf79328de8759aef
Marcin Kapelan <marcinkape@gmail.com>
2018-08-26 00:31:15 +0200

    Add tests concerning ability to like posts depending on account's status

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,7 +1,12 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -18,6 +23,8 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.DataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
@@ -25,11 +32,42 @@ public class BlogManagerTest {
     @MockBean
     UserRepository userRepository;
 
+    @MockBean
+    BlogPostRepository blogPostRepository;
+
+    @MockBean
+    LikePostRepository likePostRepository;
+
+    Long likingUserId;
+    Long postToBeLikedId;
+    User likingUser;
+
     @Autowired
     DataMapper dataMapper;
 
     @Autowired
     BlogService blogService;
+
+    @Before
+    public void setUp() {
+        likingUserId = 1000L;
+        postToBeLikedId = 500L;
+
+        likingUser = new User();
+        likingUser.setId(likingUserId);
+        likingUser.setAccountStatus(AccountStatus.NEW);
+
+        User postAuthor = new User();
+        postAuthor.setId(100900L);
+
+        BlogPost postToBeLiked = new BlogPost();
+        postToBeLiked.setId(postToBeLikedId);
+        postToBeLiked.setUser(postAuthor);
+
+        Mockito.when(userRepository.findOne(likingUserId)).thenReturn(likingUser);
+        Mockito.when(blogPostRepository.findOne(postToBeLikedId)).thenReturn(postToBeLiked);
+        Mockito.when(likePostRepository.findByUserAndPost(likingUser, postToBeLiked)).thenReturn(Optional.empty());
+    }
 
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
@@ -38,6 +76,20 @@ public class BlogManagerTest {
         Mockito.verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+
+    @Test
+    public void confirmedUserAccountShouldBeAbleToAddLikeToPost() {
+        likingUser.setAccountStatus(AccountStatus.CONFIRMED);
+
+        Assert.assertThat(blogService.addLikeToPost(likingUserId, postToBeLikedId), Matchers.is(true));
+    }
+
+    @Test (expected = DomainError.class)
+    public void newUserAccountShouldNotBeAbleToAddLikeToPost_shouldThrowDomainError() {
+        likingUser.setAccountStatus(AccountStatus.NEW);
+
+        blogService.addLikeToPost(likingUserId, postToBeLikedId);
     }
 
 }
