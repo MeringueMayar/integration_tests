4c7f2bc2c13da0919e759bb4e7e0a90caa6bff96
Marcin Kapelan <marcinkape@gmail.com>
2018-08-26 21:52:32 +0200

    Add test for email uniqueness when adding account

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -17,4 +17,12 @@ public class CreateUserTest extends FunctionalTests {
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when().post(USER_API);
     }
+
+
+    @Test
+    public void postFormWithNonUniqueEmailInRequestDataReturnsConflict() {
+        JSONObject jsonObj = new JSONObject().put("email", "john@domain.com");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CONFLICT).when().post(USER_API);
+    }
 }
