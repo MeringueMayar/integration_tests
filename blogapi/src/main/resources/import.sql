6419d24c5ecdda0d5834d6b5f57d5b55f41e198f
Marcin Kapelan <marcinkape@gmail.com>
2018-08-27 23:28:17 +0200

    Add functional tests for searching posts, prepare database

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -2,5 +2,7 @@
 insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'john@domain.com', 'John', 'Steward')
 insert into user (id, account_status, email, first_name) values (null, 'NEW', 'brian@domain.com', 'Brian')
 insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'carl@domain.com', 'Carl', 'Johnson')
+insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'kent@domain.com', 'Kent', 'Paul')
+insert into user (id, account_status, email, first_name, last_name) values (null, 'REMOVED', 'mark@domain.com', 'Mark', 'Smith')
 
 insert into blog_post values (null, 'Test entry 1', 1)
