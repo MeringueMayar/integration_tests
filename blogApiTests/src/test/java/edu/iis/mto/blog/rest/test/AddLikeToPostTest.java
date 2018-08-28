6d50981d10c073f617e4e945033789a991f3317d
Bartosz Suśniło <susnilox@gmail.com>
2018-08-28 18:42:05 +0200

    Refactor AddLikeToPostTest. Add case for new user

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikeToPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikeToPostTest.java
@@ -14,4 +14,9 @@ public class AddLikeToPostTest extends FunctionalTests {
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .expect().log().all().statusCode(HttpStatus.SC_OK).body(is("true")).when().post("/blog/user/3/like/1");
     }
+    @Test
+    public void newUserLikeSomebodyUserPost() {
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when().post("/blog/user/2/like/1");
+    }
 }
