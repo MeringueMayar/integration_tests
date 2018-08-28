a25e1203084a85006f8514251aa7e25c84f67f00
Bartosz Suśniło <susnilox@gmail.com>
2018-08-28 18:35:01 +0200

    Refactor BlogManager. Not confirmed user can not like post

--- blogapi/src/main/java/edu/iis/mto/blog/domain/BlogManager.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/BlogManager.java
@@ -43,6 +43,9 @@ public class BlogManager extends DomainService implements BlogService {
     public boolean addLikeToPost(Long userId, Long postId) {
         User user = userRepository.findOne(userId);
         BlogPost post = blogPostRepository.findOne(postId);
+        if (!user.getAccountStatus().equals(AccountStatus.CONFIRMED)) {
+            throw new DomainError("cannot like post if you are not confirmed");
+        }
         if (post.getUser().getId().equals(userId)) {
             throw new DomainError("cannot like own post");
         }
