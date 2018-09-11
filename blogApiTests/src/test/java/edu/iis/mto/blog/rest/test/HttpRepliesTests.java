8ae32d3c5f11765fadd0ef8ad89447f76ba0d404
Mayar Karkout <204641@edu.p.lodz.pl>
2018-09-11 08:03:27 +0200

    3.3 Created test for searching for nonexistent user.

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/HttpRepliesTests.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/HttpRepliesTests.java
@@ -7,12 +7,12 @@ import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 import static org.apache.http.HttpStatus.*;
 
-public class CreateUserTest extends FunctionalTests {
+public class HttpRepliesTests extends FunctionalTests {
 
     private static final String USER_API = "/blog/user";
 
     @Test
-    public void postFormWithMalformedRequestDataReturnsBadRequest() {
+    public void postFormWithMalformedRequestDataReturnsConflict() {
         JSONObject jsonObj = new JSONObject();
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .body(jsonObj.toString()).expect().log().all().statusCode(SC_CONFLICT).when().post(USER_API);
@@ -23,5 +23,12 @@ public class CreateUserTest extends FunctionalTests {
         JSONObject jsonObj = new JSONObject().put("email", "tracy@domain.com");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .body(jsonObj.toString()).expect().log().all().statusCode(SC_CREATED).when().post(USER_API);
+    }
+
+    @Test
+    public void postFormWithNonExistingUserRequestDataReturnsNotFound() {
+        JSONObject jsonObj = new JSONObject();
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(SC_NOT_FOUND).when().get("/blog/user/9");
     }
 }
