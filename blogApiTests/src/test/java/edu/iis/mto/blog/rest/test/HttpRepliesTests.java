e27358a3b15ba7d4b2e6eb77a647a665d8564aa9
Mayar Karkout <204641@edu.p.lodz.pl>
2018-09-13 14:52:36 +0200

    3.4.3 Created test for liking posts.

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/HttpRepliesTests.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/HttpRepliesTests.java
@@ -6,32 +6,41 @@ import org.junit.Test;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 import static org.apache.http.HttpStatus.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItem;
 
 public class HttpRepliesTests extends FunctionalTests {
 
     private static final String USER_API = "/blog/user";
-    private static final String CONFIRMED_USER_POST_API = "/blog/user/1/post";
-    private static final String NEW_USER_POST_API = "/blog/user/2/post";
+    private static final String CONFIRMED_USER_POST = "/blog/user/1/post";
+    private static final String NEW_USER_POST = "/blog/user/2/post";
+
+    public static final String CONFIRMED_USER_NOT_OWNER_OF_POST = "/blog/user/3/like/1";
+    public static final String NEW_USER_NOT_OWNER_OF_POST = "/blog/user/2/like/1";
+    public static final String CONFIRMED_USER_OWNER_OF_POST = "/blog/user/1/like/1";
 
     @Test
     public void postFormWithMalformedRequestDataReturnsConflict() {
         JSONObject jsonObj = new JSONObject();
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonObj.toString()).expect().log().all().statusCode(SC_CONFLICT).when().post(USER_API);
+                .body(jsonObj.toString()).expect().log().all().statusCode(SC_CONFLICT)
+                .when().post(USER_API);
     }
 
     @Test
     public void postFormWithCorrectRequestDataReturnsCreated() {
         JSONObject jsonObj = new JSONObject().put("email", "tracy@domain.com");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonObj.toString()).expect().log().all().statusCode(SC_CREATED).when().post(USER_API);
+                .body(jsonObj.toString()).expect().log().all().statusCode(SC_CREATED)
+                .when().post(USER_API);
     }
 
     @Test
     public void getFormWithNonExistingUserRequestDataReturnsNotFound() {
         JSONObject jsonObj = new JSONObject();
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonObj.toString()).expect().log().all().statusCode(SC_NOT_FOUND).when().get("/blog/user/9");
+                .body(jsonObj.toString()).expect().log().all().statusCode(SC_NOT_FOUND)
+                .when().get("/blog/user/9");
     }
 
     @Test
@@ -39,23 +48,35 @@ public class HttpRepliesTests extends FunctionalTests {
         JSONObject jsonObj1 = new JSONObject().put("email", "duplicate@domain.com");
         JSONObject jsonObj2 = new JSONObject().put("email", "duplicate@domain.com");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonObj1.toString()).expect().log().all().statusCode(SC_CREATED).when().post(USER_API);
+                .body(jsonObj1.toString()).expect().log().all().statusCode(SC_CREATED)
+                .when().post(USER_API);
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonObj2.toString()).expect().log().all().statusCode(SC_CONFLICT).when().post(USER_API);
+                .body(jsonObj2.toString()).expect().log().all().statusCode(SC_CONFLICT)
+                .when().post(USER_API);
     }
 
     @Test
     public void postFormByUserWithConfirmedAccountStatusReturnsCreated() {
         JSONObject jsonObj = new JSONObject().put("entry", "entry");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonObj.toString()).expect().log().all().statusCode(SC_CREATED).when().post(CONFIRMED_USER_POST_API);
+                .body(jsonObj.toString()).expect().log().all().statusCode(SC_CREATED)
+                .when().post(CONFIRMED_USER_POST);
     }
-    
+
     @Test
     public void postFormByUserWithNewAccountStatusReturnsBadRequest() {
         JSONObject jsonObj = new JSONObject().put("entry1", "entry1");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonObj.toString()).expect().log().all().statusCode(SC_BAD_REQUEST).when().post(NEW_USER_POST_API);
-    } 
-    
+                .body(jsonObj.toString()).expect().log().all().statusCode(SC_BAD_REQUEST)
+                .when().post(NEW_USER_POST);
+    }
+
+    @Test
+    public void likingPostByNewUserReturnsBadRequest() {
+        JSONObject jsonObj = new JSONObject();
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(SC_BAD_REQUEST).when()
+                .post(NEW_USER_NOT_OWNER_OF_POST);
+    }
+
 }
--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -1,3 +1,5 @@
 --this script initiates db for h2 db (used in test profile)
 insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'john@domain.com', 'John', 'Steward')
 insert into user (id, account_status, email, first_name) values (null, 'NEW', 'brian@domain.com', 'Brian')
+insert into user (id, account_status, email, first_name) values (null, 'CONFIRMED', 'gray@domain.com', 'Gray')
+insert into blog_post values (null, 'blog post', 1)
